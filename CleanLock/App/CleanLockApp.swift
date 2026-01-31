e446d30ea35371f89dfe69742c4f5f24eb90ce66
agravic <agravic@noreply.github.com>
2026-01-31 23:27:02 +0800

    feat: connect AppDelegate to SwiftUI app lifecycle

--- CleanLock/App/CleanLockApp.swift
+++ CleanLock/App/CleanLockApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct CleanLockApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         Settings {
             EmptyView()
